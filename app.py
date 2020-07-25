b0196d4344558fa113e8a3d87ac0b4b44694f3ee
Daniel Nunes <danielmnunes08@gmail.com>
2020-07-25 20:04:01 -0300

    First project commit

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, request
+from flask_bootstrap import Bootstrap
+import requests
+import configparser
+
+def get_api_key():
+    config = configparser.ConfigParser()
+    config.read("config.ini")
+    return config["openweathermap"]["api"]
+
+
+def get_weather_results(zip_code, api_key):
+    api_url = "https://api.openweathermap.org/data/2.5/weather?zip={}&units=imperial&appid={}".format(zip_code, api_key)
+    res = requests.get(api_url)
+    return res.json()
+
+app = Flask(__name__)
+Bootstrap(app)
+
+@app.route("/")
+def weather_dashboard():
+    return render_template("home.html")
+
+@app.route("/results", methods=["POST"])
+def render_results():
+    zip_code = request.form["zip_code"]
+    api_key = get_api_key()
+    data = get_weather_results(zip_code, api_key)
+    temp = "{0:.2f}".format(data["main"]["temp"])
+    feels_like = "{0:.2f}".format(data["main"]["feels_like"])
+    weather = data["weather"][0]["main"]
+    location = data["name"]
+
+    return render_template("results.html",
+                            temp=temp,
+                            feels_like=feels_like,
+                            weather=weather,
+                            location=location)   
+
+
+
+
+
